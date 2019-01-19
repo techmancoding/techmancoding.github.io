0aad9fbb6b30036dbbe581870093d51ae34d8219
TechMan Coding <techmancoding@google.com>
2019-01-20 01:35:44 +0200

    add jquery 3.3.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-alert("welcome to TechMane Coding site !");
+
+$(document).ready(function(){
+	$('#testJquery')
+	.html("welcome to TechMan Coding site !")
+	.css({"color":"red","text-align":"center"});
+});
