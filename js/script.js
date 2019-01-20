ebbee7ce8a8f744817ceceb6b57162f74fc887ae
TechMan Coding <techmancoding@google.com>
2019-01-20 05:14:28 +0200

    add basic footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 
 $(document).ready(function(){
-
-	$('#testJquery')
-	.html("welcome to TechMan Coding site !")
-	.css({"color":"red","text-align":"center"});
+	$('#testJquery').html("welcome to TechMan Coding site !").css({"color":"#656565","text-align":"center"});
 });
