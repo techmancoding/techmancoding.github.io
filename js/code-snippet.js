7f94db6d01169e9c81fb424d324d5cad7aa0ecbf
TechMan Coding <techmancoding@google.com>
2019-01-21 01:23:10 +0200

    add basic code snippet function

--- js/code-snippet.js
+++ js/code-snippet.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-
-	var code = $('.code-snippet pre').html();
-	alert('The code is: ' + code);
+	
+	var words = ['function', 'var', 'return'];
+	var $pre = $('.code-snippet pre');
+	var code = $pre.html();
+	
+	for(var i=0; i<words.length; i++){
+		var word = words[i];
+		code = code.replace(new RegExp(word, 'g'), '<span class="csw-' + word + '">' + word + '</span>');
+		// for each word in the array we wrap the word in a span tag with a new class starting 
+		// with csw- prefix that corresponding to the css class in code-snippet.css
+	}
+	
+	$pre.html(code);
 });
