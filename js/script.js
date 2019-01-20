05bf9d72075ecef7a4a79ef7ece3e19c0c23dd2e
TechMan Coding <techmancoding@google.com>
2019-01-20 19:14:39 +0200

    test scroll code

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
 		//alert('you clicked on menu - you need to implement this !!!');
 		$('#menu-container').toggle();
 	});
+	
+	var $root = $('html, body');	
+	$('a[href^="#"]').click(function() {
+		var href = $.attr(this, 'href');	
+		$root.animate({
+			scrollTop: $(href).offset().top
+		}, 5000, function () {
+			window.location.hash = href;
+		});	
+		return false;
+	});
 });
