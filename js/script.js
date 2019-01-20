a42af8f5c0ba7061236dc562166579a6fed42aa1
TechMan Coding <techmancoding@google.com>
2019-01-20 17:19:40 +0200

    add basic menu container

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 	$('#menu-button').on('click', function(){
-		alert('you clicked on menu - you need to implement this !!!');
+		//alert('you clicked on menu - you need to implement this !!!');
+		$('#menu-container').toggle();
 	});
 });
