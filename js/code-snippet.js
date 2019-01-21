645612a5a34b99fca99988cb8f07708783123068
TechMan Coding <techmancoding@google.com>
2019-01-21 03:07:13 +0200

    add basic reserved javascript keywords arrays

--- js/code-snippet.js
+++ js/code-snippet.js
@@ -1,15 +1,45 @@
 $(document).ready(function(){
 	
-	var words = ['function', 'var', 'return','alert'];
-	var $pre = $('.code-snippet pre');
+	/*var allKeywords =['break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue',
+	'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'eval', 'export', 'extends',
+	'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import', 'in',
+	'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'null', 'package', 'private',
+	'protected', 'public', 'return', 'short', 'static', 'super', 'switch', 'synchronized', 'this',
+	'throws', 'transient','true', 'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'];*/
+	
+	var jsKeywords =['break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue',
+	'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'eval', 'export', 'extends',
+	'final', 'finally', 'float', 'for', 'goto', 'if', 'implements', 'import', 'in',
+	'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'package', 'private',
+	'protected', 'public', 'short', 'static', 'super', 'switch', 'synchronized', 'this',
+	'throws', 'transient', 'try', 'typeof', 'void', 'volatile', 'while', 'with', 'yield'];
+	
+	var jsLiterals =['null','true','false'];
+		
+	var jsSpecialWords = ['function', 'var', 'return'];
+	
+	var $pre = $('.code-snippet-container pre');
 	var code = $pre.html();
 	
-	for(var i=0; i<words.length; i++){
-		var word = words[i];
-		code = code.replace(new RegExp(word, 'g'), '<span class="csw-' + word + '">' + word + '</span>');
-		// for each word in the array we wrap the word in a span tag with a new class starting 
-		// with csw- prefix that corresponding to the css class in code-snippet.css
-	}
+	/*for(var i=0; i<jsKeywords.length; i++){
+		var word = jsKeywords[i];
+		code = code.replace(new RegExp( "\\b" + word + "\\b", 'g'), '<span class="csw-keywords">' + word + '</span>');
+	}*/
 	
-	$pre.html(code);
+	var newCode = replaceWithRegex(code, jsKeywords, 'csw-keywords');
+	newCode = replaceWithRegex(newCode, jsLiterals, 'csw-literals');
+	newCode = replaceWithRegex(newCode, jsSpecialWords, 'csw-special');
+	
+	alert(newCode);
+	$pre.html(newCode);
 });
+
+
+
+function replaceWithRegex(codeStr, wordsArray, classToSpan){
+	for(var i=0; i<wordsArray.length; i++){
+		var word = wordsArray[i];
+		codeStr = codeStr.replace(new RegExp( "\\b" + word + "\\b", 'g'), '<span class="' + classToSpan + '">' + word + '</span>');
+	}	
+	return codeStr;
+}
