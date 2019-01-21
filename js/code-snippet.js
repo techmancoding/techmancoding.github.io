8a8c736bd29af6155b2e0c7e4184cd1a155783cd
TechMan Coding <techmancoding@google.com>
2019-01-21 17:48:52 +0200

    add string detection to snippet

--- js/code-snippet.js
+++ js/code-snippet.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
 	'throws', 'transient','true', 'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'];*/
 	
 	var jsKeywords =['break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue',
-	'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'eval', 'export', 'extends',
-	'final', 'finally', 'float', 'for', 'goto', 'if', 'implements', 'import', 'in',
+	'debugger', 'default', 'delete', 'do', 'double', 'enum', 'eval', 'export', 'extends',
+	'final', 'finally', 'float', 'goto', 'implements', 'import', 'in',
 	'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'package', 'private',
 	'protected', 'public', 'short', 'static', 'super', 'switch', 'synchronized', 'this',
-	'throws', 'transient', 'try', 'typeof', 'void', 'volatile', 'while', 'with', 'yield'];
+	'throws', 'transient', 'try', 'typeof', 'void', 'volatile', 'with', 'yield'];
 	
 	
 	var jsReservedWords = {
@@ -41,11 +41,31 @@ $(document).ready(function(){
 			'styles':'color:#d47a60;',
 			'regex':''
 		},
-	}
+		'statements' : {
+			'words':['if', 'else', 'for', 'while', 'in'],
+			'styles':'color:#355489; font-weight:bold;',
+			'regex':''
+		},
+		'string' : {
+			'words':[''],
+			'styles':'color:#289a39;',
+			'regex': /\'[^']+\'/g,			
+		},
+		/*'statements' : {
+			'words':[''],
+			'styles':'color:#a85499;',
+			'regex': /(if|else if)\([^)]+\)/g,			
+		},	
+		'loops' : {
+			'words':[''],
+			'styles':'color:#5874a9;',
+			'regex': /(for|while|do while)\([^)]+\)/g,			
+		},*/
+	}	
 	
 	var $pre = $('.code-snippet-container pre');
 	var code = $pre.html();
-	//alert(highlightWords(code, jsReservedWords));
+
 	$pre.html(highlightWords(code, jsReservedWords));
 });
 
@@ -56,10 +76,19 @@ function highlightWords(code, wordsObject){
 		var obj = wordsObject[key];
 		var words = obj.words;
 		var styles = obj.styles;
+		var regex = obj.regex;		
 		
 		for(var i=0; i<words.length; i++){
 			var word = words[i];
-			code = code.replace(new RegExp( "\\b" + word + "\\b", 'g'), '<span style="' + styles + '">' + word + '</span>');
+			if(regex){
+				var matches = code.match(regex);				
+				for(var m=0; m<matches.length; m++){
+					code = code.replace(matches[m], '<span style="' + styles + '">' + matches[m] + '</span>');
+				}
+			} 
+			else{		
+				code = code.replace(new RegExp( "\\b" + word + "\\b", 'g'), '<span style="' + styles + '">' + word + '</span>');	
+			}
 		}	
 	}
 	
