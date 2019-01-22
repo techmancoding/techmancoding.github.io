c7a8cfd8763fdd5d1934572401739628cd9d7ab1
TechMan Coding <techmancoding@google.com>
2019-01-22 14:53:11 +0200

    add basic blog section

---
+++ js/blogs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var $container = $('#blogs-container');
+	
+	$container.append('<div class="blog-container blog-bg-vertical"><div class="blog-main-title-one">CSS3 Tutorial</div><div class="blog-sub-title-one">Learn CSS3 for modern web design</div><div class="blog-summary-one">CCS3 is the latest version of CSS language (Cascading Style Sheets). in this course we will learn how to apply CSS3 to your website HTML and make your site look awesome.</div><div class="blog-img-container"><img src="./img/logo.png" class="blog-img"/></div></div>');
+	$container.append('<div class="blog-container blog-bg-vertical"><div class="blog-main-title-two">Javascript Tutorial</div><div class="blog-sub-title-two">javascript ES5</div><div class="blog-summary-two">JavaScript language can make your site better. in this course we will learn how to implement JavaScript language to make your website more dynamic.</div><div class="blog-img-container"><img src="./img/logo.png" class="blog-img"/></div></div>');
+	$container.append('<div class="blog-container blog-bg-horizontal">second blog</div>');
+	$container.append('<div class="blog-container blog-bg-vertical">third blog</div>');
+	$container.append('<div class="blog-container blog-bg-horizontal">fourth blog</div>');
+	$container.append('<div class="blog-container blog-bg-vertical">fifth blog</div>');
+	$container.append('<div class="blog-container blog-bg-horizontal">sixth blog</div>');
+});
