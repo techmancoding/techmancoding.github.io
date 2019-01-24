4f12ae8e355758079ecfc3bdb1c0506ecda41594
TechMan Coding <techmancoding@google.com>
2019-01-24 17:09:52 +0200

    add selected menu item

--- js/menu-script.js
+++ js/menu-script.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
 
 	var $nav = $('nav');	
 	var relativePath = $('resources').attr('path');
+	var pageKey = $('resources').attr('pagekey'); // need to be the same as the key on anchorsObject
 	$nav.append('<div><a href="/"><img src="' + relativePath + '/img/logo.png" class="nav-logo"/></a></div>');
 	$nav.append('<div id="menu-button" class="disable-select">&#9776;</div>'); // 	&#9784;   	&#9763;   	&#9762;
 	$nav.append('<div id="menu-container" style="display:none;"></div>');   // 	&#5121;.     	&#5123;
@@ -29,34 +30,12 @@ $(document).ready(function(){
 	var $button = $('#menu-button');
 	
 	for(var key in anchorsObject){
-		var anchor = anchorsObject[key];		
-		var anchorSelected = ''; // TODO !!!
-		$menu.append('<a href="' + anchor.href + '"><div class="menu-item-container">' + anchor.text +'</div></a>');
+		var anchor = anchorsObject[key];
+		var menuItemSelected = (key == pageKey) ? 'menu-item-selected' : '';		
+		$menu.append('<a href="' + anchor.href + '"><div class="menu-item-container ' + menuItemSelected + '">' + anchor.text +'</div></a>');
 	}
 
 	$button.on('click', function(){
 		$menu.toggle();
 	});
-	/*var $nav = $('nav');
-		
-	var relativePath = $('resources').attr('path');
-	var anchorsArray = $('navmenu').attr('anchors').split(';');
-	
-	$nav.append('<div><a href="/"><img src="' + relativePath + '/img/logo.png" class="nav-logo"/></a></div>');
-	$nav.append('<div id="menu-button" class="disable-select">&#9776;</div>'); // 	&#9784;   	&#9763;   	&#9762;
-	$nav.append('<div id="menu-container" style="display:none;"></div>');   // 	&#5121;.     	&#5123;
-	
-	var $menu = $('#menu-container');
-	
-	for(var i=0; i<anchorsArray.length; i++){
-		var anchor = anchorsArray[i].replace('[','').replace(']','').split(',');
-		var aHref = anchor[0];
-		var aText = anchor[1];
-		var aShadow = anchor[2];
-		$menu.append('<a href="' + aHref + '"><div class="menu-item-container ' + aShadow + '">' + aText +'</div></a>');
-	}
-
-	$('#menu-button').on('click', function(){
-		$menu.toggle();
-	});*/
 });
