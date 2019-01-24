f49055c3551f20f755907749324be45754bf59e1
TechMan Coding <techmancoding@google.com>
2019-01-24 20:01:23 +0200

    add sub menu button

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
 	var anchorsObject = {
@@ -9,6 +8,20 @@ $(document).ready(function(){
 		'tutorials' : {
 			'href' : '/tutorials',
 			'text' : 'Tutorials',
+			'submenu' : {
+				'html5': {
+					'href' : '/tutorials/tags/html5-tutorials',
+					'text' : 'HTML5'
+				},
+				'css3': {
+					'href' : '/tutorials/tags/css3-tutorials',
+					'text' : 'CSS3'
+				},
+				'javascript': {
+					'href' : '/tutorials/tags/javascript-tutorials',
+					'text' : 'Javascript'
+				}
+			}
 		},
 		'empty-template' :{
 			'href' : '/tutorials/empty-template',
@@ -18,7 +31,7 @@ $(document).ready(function(){
 			'href' : '/#section-five',
 			'text' : 'five section'
 		}
-	}
+	};
 
 	var $nav = $('nav');	
 	var relativePath = $('resources').attr('path');
@@ -27,15 +40,39 @@ $(document).ready(function(){
 	$nav.append('<div id="menu-button" class="disable-select">&#9776;</div>'); // 	&#9784;   	&#9763;   	&#9762;
 	$nav.append('<div id="menu-container" style="display:none;"></div>');   // 	&#5121;.     	&#5123;
 	var $menu = $('#menu-container');
-	var $button = $('#menu-button');
+	var $menuButton = $('#menu-button');
+	var counter = 0;
 	
 	for(var key in anchorsObject){
-		var anchor = anchorsObject[key];
-		var menuItemSelected = (key == pageKey) ? 'menu-item-selected' : '';		
-		$menu.append('<a href="' + anchor.href + '"><div class="menu-item-container ' + menuItemSelected + '">' + anchor.text +'</div></a>');
+		var menuItem = anchorsObject[key];
+		var menuItemSelected = (key == pageKey) ? 'menu-item-selected' : '';
+				
+		if(!menuItem.submenu){		
+			$menu.append('<a href="' + menuItem.href + '"><div class="menu-item-container ' + menuItemSelected + '">' + menuItem.text +'</div></a>');
+		}
+		else{ // menu item contains sub menu
+			var subMenuId = 'sub-menu-id-' + counter;
+			var subMenuButtonId = 'sub-menu-button-id-' + counter;
+			var subMenu = '<div id="' + subMenuId + '" class="sub-menu-container" style="display:none;">';
+			for(var subKey in menuItem.submenu){
+				var subMenuItem = menuItem.submenu[subKey];
+				subMenu += '<a href="' + subMenuItem.href + '"><div class="sub-menu-item-container ' + menuItemSelected + '">' + subMenuItem.text +'</div></a>';
+			}
+			subMenu += '</div>';
+			$menu.append('<a href="' + menuItem.href + '"><div class="menu-item-container-with-sub-button ' + menuItemSelected + '">' + menuItem.text +'</div></a><div id="' + subMenuButtonId + '" class="sub-menu-button"><div class="disable-select">&#9763;</div></div>');
+			$menu.append(subMenu);
+			var $subMenu = $('#' + subMenuId);
+			var $subMenuButton = $('#' + subMenuButtonId);
+			$subMenu.append('<div style="clear:both;"></div>');
+			$subMenuButton.on('click', function(){
+				$subMenu.toggle();
+			});
+		}
+		counter++;		
+		//$menu.append('<a href="' + menuItem.href + '"><div class="menu-item-container ' + menuItemSelected + '">' + menuItem.text +'</div></a>');
 	}
 
-	$button.on('click', function(){
+	$menuButton.on('click', function(){
 		$menu.toggle();
 	});
 });
