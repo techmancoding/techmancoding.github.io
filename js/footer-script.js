4f263ff21f037748e26344692c9e741fab0c6562
TechMan Coding <techmancoding@google.com>
2019-01-25 03:33:52 +0200

    add footer basic menu

--- js/footer-script.js
+++ js/footer-script.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
 
 	var relativePath = $('resources').attr('path');
-	
+		
 	var $footer = $('footer');
 	$footer.append('<div class="footer-text">TechMan Coding &#xA9; 2019</div>');
 	$footer.append('<div class="footer-text">Contact info</div>');
-	$footer.append('<div><img src="' + relativePath + '/img/logo.png" class="footer-logo"/></div>');	
+	//$footer.append('<div><img src="' + relativePath + '/img/logo.png" class="footer-logo"/></div>');	
+	
+	$footer.append('<div id="footer-menu-container"></div>');
+	var $menu = $('#footer-menu-container');
+	
+	for(var key in MenuItemsJSON){
+		var menuItem = MenuItemsJSON[key];
+		$menu.append('<a href="' + menuItem.href + '"><div class="footer-menu-item">' + menuItem.text + '</div></a>');
+		
+		if(menuItem.submenu){				
+			for(var subKey in menuItem.submenu){
+				var subMenuItem = menuItem.submenu[subKey];
+				$menu.append('<a href="' + subMenuItem.href + '"><div class="footer-sub-menu-item">' + subMenuItem.text + '</div></a>');
+			}		
+		}		
+	}
 });
